3268597da3c7aa5b3d2634f77726876d2318a002
dhanushkasandakelum <dhanushkasandakelum711@gmail.com>
2022-07-05 10:50:30 +0530

    Initial services defined

---
+++ backend-ms/selection-process/src/main/java/com/ugc/selections/SelectionProcessApplication.java
@@ -0,0 +1,11 @@
+package com.ugc.selections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SelectionProcessApplication.class, args);
+    }
+}
---
+++ backend-ms/zscore/src/main/java/com/ugc/zscore/ZScoreApplication.java
@@ -0,0 +1,11 @@
+package com.ugc.zscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZScoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZScoreApplication.class, args);
+    }
+}
