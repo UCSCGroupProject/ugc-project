f7595f0dc94ffe8718a5d468b1861704635ee37d
dhanushkasandakelum <dhanushkasandakelum711@gmail.com>
2022-07-05 12:59:27 +0530

    Eureka server and client implemented

--- backend-ms/zscore/src/main/java/com/ugc/zscore/ZScoreApplication.java
+++ backend-ms/zscore/src/main/java/com/ugc/zscore/ZScoreApplication.java
@@ -2,8 +2,10 @@ package com.ugc.zscore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ZScoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZScoreApplication.class, args);
