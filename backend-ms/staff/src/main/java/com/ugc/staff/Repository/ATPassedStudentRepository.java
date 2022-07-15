a301d7555f898f27978922c83725c08302717cce
ilmabf <2019cs061@stu.ucsc.lk>
2022-07-15 22:41:17 +0530

    Staff routing for ALPassed, ATPassed and Applied students

---
+++ backend-ms/staff/src/main/java/com/ugc/staff/Repository/ATPassedStudentRepository.java
@@ -0,0 +1,7 @@
+package com.ugc.staff.Repository;
+
+import com.ugc.staff.Model.ATPassedStudent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ATPassedStudentRepository extends JpaRepository<ATPassedStudent, Long> {
+}
