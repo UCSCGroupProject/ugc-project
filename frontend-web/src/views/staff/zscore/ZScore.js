979f2b4bac2e2d888e2cdbc39c20c5e34b3a4b19
Pabasara Ashvini <pabasaraasvini12@gmail.com>
2022-07-23 20:46:12 +0530

    UI-zscore add table

--- frontend-web/src/views/staff/zscore/ZScore.js
+++ frontend-web/src/views/staff/zscore/ZScore.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   CButton,
-  CCard,
-  CCardBody,
-  CCol,
-  CContainer,
-  CForm,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter,
+  CFormCheck,
   CFormInput,
-  CRow,
-  CSpinner,
-  CAlert,
 } from '@coreui/react'
 
-
 function StaffZScore() {
+  const [visible, setVisible] = useState(false)
   return (
-    <><div>StaffZScore</div>
+    <>
+      <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+        <CButton color="primary" className="me-md-2" onClick={() => setVisible(!visible)}>
+          Add Table
+        </CButton>
+        <CModal alignment="center" visible={visible} onClose={() => setVisible(false)}>
+          <CModalHeader>
+            <CModalTitle>Import Z-Score Table</CModalTitle>
+          </CModalHeader>
+          <CModalBody>       
+            <div>
+              <CFormInput type="file" size="sm" id="formFileSm" label="Choose your file to import as Z-score table" />
+            </div>
+            <div><CFormInput type="text" size="sm" label="Save as" text="Please enter the year of Z-score table" aria-label="sm input example"/></div>
+            {/* <div><CFormCheck id="flexCheckChecked" label="Latest Z-table" /></div> */}
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setVisible(false)}>
+              Close
+            </CButton>
+            <CButton color="primary">Import</CButton>
+          </CModalFooter>
+        </CModal>
+        
+      </div>
       <div className="d-grid gap-2 col-6 mx-auto">
         <CButton color="primary" size="lg" type="button" variant="outline" href="#/staff/zscoretable">
             2021 - Latest
