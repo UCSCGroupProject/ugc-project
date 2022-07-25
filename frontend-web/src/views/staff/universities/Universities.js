e3fb0b8ea032dd81c935580cfca5385e2ccd7b0c
ilmabf <2019cs061@stu.ucsc.lk>
2022-07-25 11:56:38 +0530

    staff view universities

--- frontend-web/src/views/staff/universities/Universities.js
+++ frontend-web/src/views/staff/universities/Universities.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {
   CTable,
-  CButton,
+  CButton
 } from '@coreui/react'
-
+import logo from '../../../assets/images/logo-uoc.png';
 function StaffUniversities() {
 
   const columns = [
@@ -18,10 +18,9 @@ function StaffUniversities() {
       _props: { scope: 'col' },
     },
     {
-      key: 'show',
-      label: 'action',
-      _style: { width: '10%' },
-      _props: { color: 'primary', className: 'fw-semibold' },
+      key: 'logo',
+      label: '',
+      _props: { scope: 'col' },
     },
   ]
 
@@ -30,124 +29,125 @@ function StaffUniversities() {
       id: 1,
       class: 'university',
       name: 'University of Colombo',
-      show: <CButton>view Profile</CButton>,
-      _cellProps: { id: { scope: 'row' } },
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
+      _cellProps: { id: { scope: 'row'} },
     },
     {
       id: 2,
       class: 'university',
       name: 'Eastern University',
-      show: <CButton>view Profile</CButton>,
-      _cellProps: { id: { scope: 'row' } },
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
+      _cellProps: { id: { scope: 'row'} },
     },
     {
       id: 3,
       class: 'university',
       name: 'University of Jaffna',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 4,
       class: 'university',
       name: 'University of Kelaniya',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 5,
       class: 'university',
       name: 'University of Moratuwa',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 6,
       class: 'university',
       name: 'Open University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 7,
       class: 'university',
       name: 'University of Peradeniya',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 8,
       class: 'university',
       name: 'Rajarata University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 9,
       class: 'university',
       name: 'University of Ruhuna',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 10,
       class: 'university',
       name: 'Sabaragamuwa University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 11,
       class: 'university',
       name: 'South Eastern University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 12,
       class: 'university',
       name: 'University of Sri Jayewardenepura',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 13,
       class: 'university',
       name: 'Uva Wellassa University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 14,
       class: 'university',
       name: 'University of the Visual and Performing Arts',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 15,
       class: 'university',
       name: 'Wayamba University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 16,
       class: 'university',
       name: 'Gampaha Wickramarachchi University',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt='' ></img>,
       _cellProps: { id: { scope: 'row' } },
     },
     {
       id: 17,
       class: 'university',
       name: 'University of Vavuniya',
-      show: <CButton>view Profile</CButton>,
+      logo: <img src={logo} width={30} height={35} alt=''></img>,
       _cellProps: { id: { scope: 'row' } },
     },
   ]
 
-  return <CTable responsive="md" captionTop="List of universities" columns={columns} 
+
+  return <CTable  hover responsive captionTop="List of universities" columns={columns} 
   items={items} tableHeadProps={{ color: 'dark' }}/>
 }
 
