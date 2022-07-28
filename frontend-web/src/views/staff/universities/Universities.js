809ffe0ba754dc1b55b0cd861da5c9d042f6ce07
ilmabf <2019cs061@stu.ucsc.lk>
2022-07-28 14:59:20 +0530

    Update Universities.js

--- frontend-web/src/views/staff/universities/Universities.js
+++ frontend-web/src/views/staff/universities/Universities.js
@@ -1,40 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {
-  CLink,
+  CCard,
   CTable,
   CCol,
-  CNavLink,
-  CNavItem,
   CTableHead,
+  CFormInput,
+  CCardBody,
+  CButton,
+  CFormSelect,
+  CCardHeader,
   CTableRow,
+  CInputGroupText,
   CTableHeaderCell,
+  CInputGroup,
   CRow,
   CTableBody,
   CTableDataCell,
 } from '@coreui/react'
+
+import { cilSearch } from '@coreui/icons'
+import { cilFilter } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
+
 import logo from '../../../assets/images/logo-uoc.png'
 function StaffUniversities() {
-  {
-    /* <CNav><CNavItem><CNavLink  href="/staff/univerityprofile" active/></CNavItem></CNav> */
-  }
-  const columns = [
-    {
-      key: 'id',
-      label: '#',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'name',
-      label: 'Name',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'logo',
-      label: '',
-      _props: { scope: 'col' },
-    },
-  ]
 
   const universityDetails = [
     {
@@ -158,35 +148,73 @@ function StaffUniversities() {
     },
   ]
 
-  {
-    /* <NavLink
-            to="/staff/univerityprofile"
-            style={{ textDecoration: 'none', color: 'inherit' }}
-          > */
-  }
-
-  
   return (
-    <CTable bordered>
-      <CTableHead color="dark">
-        <CTableRow>
-          <CTableHeaderCell>Id</CTableHeaderCell>
-          <CTableHeaderCell>Name</CTableHeaderCell>
-          <CTableHeaderCell>Logo</CTableHeaderCell>
-        </CTableRow>
-      </CTableHead>
-      <CTableBody>
-        {universityDetails.map((item) => (
-          // <NavLink to="/staff/univerityprofile" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <CTableRow key={item.id} >
-              <CTableHeaderCell>{item.id}</CTableHeaderCell>
-              <CTableDataCell>{item.name}</CTableDataCell>
-              <CTableDataCell>{item.logo}</CTableDataCell>
-            </CTableRow>
-          
-        ))}
-      </CTableBody>
-    </CTable>
+    <div>
+      <CRow>
+        <CCol xs>
+          <CCard className="mb-4">
+            <CCardHeader>All Universities</CCardHeader>
+            <CCardBody>
+              <CRow className="py-2 bg-light rounded">
+                <CCol md={6}>
+                  <CInputGroup>
+                    <CInputGroupText>Filter By</CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="all">All</option>
+                      <option value="unicode">Unicode</option>
+                      <option value="course">Course</option>
+                      <option value="university">University</option>
+                    </CFormSelect>
+                    <CInputGroupText> in </CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="ascending">Ascending</option>
+                      <option value="descending">Descending</option>
+                    </CFormSelect>
+                    <CInputGroupText> order </CInputGroupText>
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilFilter} />
+                      <span>{'  '}Filter</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+
+                <CCol md={4} className="ms-auto">
+                  <CInputGroup>
+                    <CFormInput type="text" name="phone" placeholder="Search..." />
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilSearch} />
+                      <span>{'  '}Search</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+              </CRow>
+              <br />
+              <CRow className="m-1">
+                <CTable bordered>
+                  <CTableHead color="dark">
+                    <CTableRow>
+                      <CTableHeaderCell>Id</CTableHeaderCell>
+                      <CTableHeaderCell>Name</CTableHeaderCell>
+                      <CTableHeaderCell>Logo</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    {universityDetails.map((item) => (
+                      // <NavLink to="/staff/univerityprofile" style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <CTableRow key={item.id}>
+                        <CTableHeaderCell>{item.id}</CTableHeaderCell>
+                        <CTableDataCell>{item.name}</CTableDataCell>
+                        <CTableDataCell>{item.logo}</CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </div>
   )
 }
 
