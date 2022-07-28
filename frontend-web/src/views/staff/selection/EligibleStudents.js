805a46b70370eea550eeef5f020078daf19201a9
ilmabf <2019cs061@stu.ucsc.lk>
2022-07-28 23:38:42 +0530

    Staff view of eligible students for university courses

--- frontend-web/src/views/staff/selection/EligibleStudents.js
+++ frontend-web/src/views/staff/selection/EligibleStudents.js
@@ -1,8 +1,122 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {
+  CCard,
+  CTable,
+  CCol,
+  CTableHead,
+  CFormInput,
+  CCardBody,
+  CButton,
+  CFormSelect,
+  CCardHeader,
+  CTableRow,
+  CInputGroupText,
+  CTableHeaderCell,
+  CInputGroup,
+  CRow,
+  CTableBody,
+  CTableDataCell,
+} from '@coreui/react'
+
+import { cilSearch } from '@coreui/icons'
+import { cilFilter } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
 
 function EligibleStudents() {
+
+  const eligibleStudentsList = [
+    {
+      id: 1,
+      unicode: '112A',
+      course: 'Medicine',
+      university: 'University of Colombo',
+      intake: '200',
+      eligibleCount: '500',
+      _cellProps: { id: { scope: 'row' } },
+    },
+    {
+      id: 1,
+      unicode: '112A',
+      course: 'Engineering',
+      university: 'University of Moratuwa',
+      intake: '200',
+      eligibleCount: '500',
+      _cellProps: { id: { scope: 'row' } },
+    }
+  ]
+
   return (
-    <div>EligibleStudents</div>
+    <div>
+      <CRow>
+        <CCol xs>
+          <CCard className="mb-4">
+            <CCardHeader>Students eligible for degree programmes</CCardHeader>
+            <CCardBody>
+              <CRow className="py-2 bg-light rounded">
+                <CCol md={6}>
+                  <CInputGroup>
+                    <CInputGroupText>Filter By</CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="all">All</option>
+                      <option value="course">Course</option>
+                      <option value="university">University</option>
+                      <option value="zscore">Intake</option>
+                      <option value="school">Eligible Count</option>
+                    </CFormSelect>
+                    <CInputGroupText> in </CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="ascending">Ascending</option>
+                      <option value="descending">Descending</option>
+                    </CFormSelect>
+                    <CInputGroupText> order </CInputGroupText>
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilFilter} />
+                      <span>{'  '}Filter</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+                <CCol md={4} className="ms-auto">
+                  <CInputGroup>
+                    <CFormInput type="text" name="phone" placeholder="Search..." />
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilSearch} />
+                      <span>{'  '}Search</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+              </CRow>
+              <br />
+              <CRow className="m-1">
+                <CTable bordered>
+                  <CTableHead color="dark">
+                    <CTableRow>
+                      <CTableHeaderCell>Unicode</CTableHeaderCell>
+                      <CTableHeaderCell>Course</CTableHeaderCell>
+                      <CTableHeaderCell>University</CTableHeaderCell>
+                      <CTableHeaderCell>Intake</CTableHeaderCell>
+                      <CTableHeaderCell>No. of Eligible Students</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    {eligibleStudentsList.map((item) => (
+                      // <NavLink to="/staff/univerityprofile" style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <CTableRow key={item.id}>
+                        <CTableHeaderCell>{item.unicode}</CTableHeaderCell>
+                        <CTableDataCell>{item.course}</CTableDataCell>
+                        <CTableDataCell>{item.university}</CTableDataCell>
+                        <CTableDataCell>{item.intake}</CTableDataCell>
+                        <CTableDataCell>{item.eligibleCount}</CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </div>
   )
 }
 
