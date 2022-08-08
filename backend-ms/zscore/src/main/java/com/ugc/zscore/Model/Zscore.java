4a64e0668901c4f1325f63c089217e10c2645868
Pabasara Ashvini <pabasaraasvini12@gmail.com>
2022-08-08 14:02:18 +0530

    database connection issue fixed

--- backend-ms/zscore/src/main/java/com/ugc/zscore/Model/Zscore.java
+++ backend-ms/zscore/src/main/java/com/ugc/zscore/Model/Zscore.java
@@ -5,11 +5,15 @@ import lombok.Setter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "zscore")
 @Getter
 @Setter
 public class Zscore {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
