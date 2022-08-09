1b408246c282a71b437914ada0c3d837d5a87aad
Pabasara Ashvini <pabasaraasvini12@gmail.com>
2022-08-09 13:16:00 +0530

    import .csv file part done

--- backend-ms/zscore/src/main/java/com/ugc/zscore/Repository/ZscoreRepository.java
+++ backend-ms/zscore/src/main/java/com/ugc/zscore/Repository/ZscoreRepository.java
@@ -1,7 +1,9 @@
 package com.ugc.zscore.Repository;
 
+import com.ugc.zscore.Model.Zscore;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ZscoreRepository {
+public interface ZscoreRepository extends JpaRepository<Zscore, Long> {
 }
--- backend-ms/zscore/src/main/java/com/ugc/zscore/Services/ZscoreService.java
+++ backend-ms/zscore/src/main/java/com/ugc/zscore/Services/ZscoreService.java
@@ -1,7 +1,39 @@
 package com.ugc.zscore.Services;
 
+import com.ugc.zscore.Helper.CSVHelper;
+import com.ugc.zscore.Model.Zscore;
+import com.ugc.zscore.Repository.ZscoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class ZscoreService {
+
+    @Autowired
+    ZscoreRepository repository;
+
+    public void save(MultipartFile file) {
+        try {
+            List<Zscore> tutorials = CSVHelper.csvToTutorials(file.getInputStream());
+            repository.saveAll(tutorials);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+    public ByteArrayInputStream load() {
+        List<Zscore> tutorials = repository.findAll();
+
+        ByteArrayInputStream in = CSVHelper.tutorialsToCSV(tutorials);
+        return in;
+    }
+
+    public List<Zscore> getAllTutorials() {
+        return repository.findAll();
+    }
 }
