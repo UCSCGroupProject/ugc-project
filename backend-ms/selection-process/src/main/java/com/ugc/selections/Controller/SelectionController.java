f68d554520219baee813e1eb41c6387d5af622a0
ilmabf <2019cs061@stu.ucsc.lk>
2022-08-15 21:17:45 +0530

    Selection - separate students of each stream

--- backend-ms/selection-process/src/main/java/com/ugc/selections/Controller/SelectionController.java
+++ backend-ms/selection-process/src/main/java/com/ugc/selections/Controller/SelectionController.java
@@ -3,10 +3,8 @@ package com.ugc.selections.Controller;
 import com.ugc.selections.Entity.SelectedStudent;
 import com.ugc.selections.Payload.Request.ALPassedRequest;
 import com.ugc.selections.Payload.Request.ApplicantRequest;
-import com.ugc.selections.Payload.Request.StreamRequest;
 import com.ugc.selections.Service.SelectionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,7 +43,25 @@ public class SelectionController  {
         // Filter and get the students who are eligible by A/L Results
         Map<String, String> eligibleStudents = selectionService.getEligible(applicants, alResultRequest);
 
-        // Execute selection algo for each stream
+        // Separate students of each stream
+        List<String> physicsStream = eligibleStudents.entrySet()
+                .stream().distinct().filter(e-> e.getValue() == "Physics")
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .values().stream().toList();
 
+        List<String> biologyStream = eligibleStudents.entrySet()
+                .stream().distinct().filter(e-> e.getValue() == "Biology")
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .values().stream().toList();
+
+        List<String> commerceStream = eligibleStudents.entrySet()
+                .stream().distinct().filter(e-> e.getValue() == "Commerce")
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .values().stream().toList();
+
+        List<String> artStream = eligibleStudents.entrySet()
+                .stream().distinct().filter(e-> e.getValue() == "Art")
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .values().stream().toList();
     }
 }
