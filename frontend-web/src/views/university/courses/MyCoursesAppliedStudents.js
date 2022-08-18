067aca0f1307d7edb1636f4076c5ce20ecb3cc22
Risini1998 <risinipiyathma1998@gmail.com>
2022-08-18 17:28:20 +0530

    Modified test result UI

--- frontend-web/src/views/university/courses/MyCoursesAppliedStudents.js
+++ frontend-web/src/views/university/courses/MyCoursesAppliedStudents.js
@@ -1,8 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  CRow,
+  CCol,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CButton,
+  CFormSelect,
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableDataCell,
+  CTableBody,
+  CInputGroup,
+  CFormInput,
+  CInputGroupText,
+} from '@coreui/react'
+
+import { cilSearch } from '@coreui/icons'
+import { cilFilter } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
 
 const MyCoursesAppliedStudents = () => {
+  const [allCoursesData, setAllCoursesData] = useState([
+    {
+      id: '1',
+      unicode: '112A',
+      courseOfStudy: 'Medicine',
+      appliedAmount: '250'
+    },
+    {
+      id: '2',
+      unicode: '112A',
+      courseOfStudy: 'Engineering',
+      appliedAmount: '400'
+    },
+    {
+      id: '3',
+      unicode: '112A',
+      courseOfStudy: 'Computer Science',
+      appliedAmount: '300'
+    },
+  ])
+
   return (
-    <div>Course Applied Students</div>
+    <div>
+      <CRow>
+        <CCol xs>
+          <CCard className="mb-4">
+            <CCardHeader>Applied Students for Courses</CCardHeader>
+            <CCardBody>
+              <CRow className="py-2 bg-light rounded">
+                <CCol md={6}>
+                  <CInputGroup>
+                    <CInputGroupText>Filter By</CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="all">All</option>
+                      <option value="unicode">Unicode</option>
+                      <option value="course">Course</option>
+                      {/* <option value="university">University</option> */}
+                    </CFormSelect>
+                    <CInputGroupText> in </CInputGroupText>
+                    <CFormSelect aria-label="filterByOption1">
+                      <option value="ascending">Ascending</option>
+                      <option value="descending">Descending</option>
+                    </CFormSelect>
+                    <CInputGroupText> order </CInputGroupText>
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilFilter} />
+                      <span>{'  '}Filter</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+                <CCol md={4} className="ms-auto">
+                  <CInputGroup>
+                    <CFormInput type="text" name="phone" placeholder="Search..." />
+                    <CButton color="warning" type="button" className="text-white">
+                      <CIcon icon={cilSearch} />
+                      <span>{'  '}Search</span>
+                    </CButton>
+                  </CInputGroup>
+                </CCol>
+              </CRow>
+              <br />
+
+              <CRow className="m-1">
+                <CTable bordered>
+                  <CTableHead color="dark">
+                    <CTableRow>
+                      <CTableHeaderCell>No.</CTableHeaderCell>
+                      <CTableHeaderCell>Unicode</CTableHeaderCell>
+                      <CTableHeaderCell>Course</CTableHeaderCell>
+                      <CTableHeaderCell>No.of Applicants</CTableHeaderCell>
+                      {/* <CTableHeaderCell>University</CTableHeaderCell> */}
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    {allCoursesData.map((item) => (
+                      <CTableRow key={item.id}>
+                        <CTableHeaderCell>{item.id}</CTableHeaderCell>
+                        <CTableDataCell>{item.unicode}</CTableDataCell>
+                        <CTableDataCell>{item.courseOfStudy}</CTableDataCell>
+                        <CTableDataCell>{item.appliedAmount}</CTableDataCell>
+                        {/* <CTableDataCell>{item.university}</CTableDataCell> */}
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </div>
   )
 }
 
