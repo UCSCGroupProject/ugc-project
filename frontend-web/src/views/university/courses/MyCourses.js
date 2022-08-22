8cc89db0b51a46a53ce6ba8d0a5960e168cc4261
Risini1998 <risinipiyathma1998@gmail.com>
2022-08-22 12:55:52 +0530

    Added Course Edit popup

--- frontend-web/src/views/university/courses/MyCourses.js
+++ frontend-web/src/views/university/courses/MyCourses.js
@@ -17,6 +17,15 @@ import {
   CInputGroup,
   CFormInput,
   CInputGroupText,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter,
+  CForm,
+  CFormLabel,
+  CFormCheck,
+  CFormTextarea,
 } from '@coreui/react'
 
 import { cilSearch } from '@coreui/icons'
@@ -25,12 +34,12 @@ import { cilPencil } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
 const MyCourses = () => {
-  const navigate = useNavigate()
+  const [visible, setVisible] = useState(false)
 
   const [allCoursesData, setAllCoursesData] = useState([
     {
       id: '1',
-      unicode: '112A',
+      unicode: '112N',
       courseOfStudy: 'Medicine',
     },
     {
@@ -39,7 +48,7 @@ const MyCourses = () => {
       courseOfStudy: 'Medicine',
     },
   ])
-
+  const [currentItem, setCurrentItem] = useState(null)
   return (
     <div>
       <CRow>
@@ -104,14 +113,13 @@ const MyCourses = () => {
                               color="btn btn-primary btn-sm"
                               type="button"
                               className="text-white"
-                              // onClick={() => {navigate('/university/courses/my/edit');}}
                               onClick={() => {
-                                var popup = document.getElementById('mypopup')
-                                popup.classList.toggle('show')
+                                setCurrentItem(item)
+                                setVisible(!visible)
                               }}
                             >
                               <CIcon icon={cilPencil} />
-                              <span> Edit</span>
+                              <span>{'  '}Edit</span>
                             </CButton>
                           </div>
                         </CTableDataCell>
@@ -124,6 +132,263 @@ const MyCourses = () => {
           </CCard>
         </CCol>
       </CRow>
+      <CModal scrollable visible={visible} onClose={() => setVisible(false)}>
+        <CModalHeader>
+          <CModalTitle>Course Edit</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CForm>
+            <CRow className="mb-3">
+              <CFormLabel htmlFor="inputunicode" className="col-sm-3 col-form-label">
+                Uni Code
+              </CFormLabel>
+              <CCol sm={12}>
+                <CFormInput
+                  type="text"
+                  id="inputunicode"
+                  value={currentItem ? currentItem.unicode : 'loading...'}
+                />
+              </CCol>
+            </CRow>
+            <CRow className="mb-3">
+              <CFormLabel htmlFor="inputcourse" className="col-sm-3 col-form-label">
+                Course
+              </CFormLabel>
+              <CCol sm={12}>
+                <CFormInput
+                  type="text"
+                  id="inputcourse"
+                  value={currentItem ? currentItem.courseOfStudy : 'loading...'}
+                />
+              </CCol>
+            </CRow>
+            <CRow className="mb-3">
+              <CFormLabel htmlFor="inputdegree" className="col-sm-3 col-form-label">
+                Degree
+              </CFormLabel>
+              <CCol sm={12}>
+                <CFormInput type="text" id="inputdegree" />
+              </CCol>
+            </CRow>
+            <CRow className="mb-3">
+              <CFormLabel htmlFor="inputreq" className="col-sm-3 col-form-label">
+                Requirements
+              </CFormLabel>
+              <CRow>
+                <CCol sm={5}>
+                  <CFormSelect size="sm" className="mb-2" aria-label="Small select example">
+                    <option>Select a Subject</option>
+                    <option value="1">Combined Mathematics</option>
+                    <option value="2">Chemistry</option>
+                    <option value="3">Physics</option>
+                    <option value="4">ICT</option>
+                    <option value="5">Bio Science</option>
+                    <option value="6">Agriculture</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol sm={7}>
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq1"
+                    id="radioreq1"
+                    value="option1"
+                    label="A"
+                    defaultChecked
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq1"
+                    id="radioreq2"
+                    value="option2"
+                    label="B"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq1"
+                    id="radioreq3"
+                    value="option3"
+                    label="C"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq1"
+                    id="radioreq4"
+                    value="option4"
+                    label="S"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq1"
+                    id="radioreq5"
+                    value="option5"
+                    label="F"
+                  />
+                </CCol>
+              </CRow>
+              <CRow>
+                <CCol sm={5}>
+                  <CFormSelect size="sm" className="mb-2" aria-label="Small select example">
+                    <option>Select a Subject</option>
+                    <option value="1">Combined Mathematics</option>
+                    <option value="2">Chemistry</option>
+                    <option value="3">Physics</option>
+                    <option value="4">ICT</option>
+                    <option value="5">Bio Science</option>
+                    <option value="6">Agriculture</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol sm={7}>
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq2"
+                    id="radioreq1"
+                    value="option1"
+                    label="A"
+                    defaultChecked
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq2"
+                    id="radioreq2"
+                    value="option2"
+                    label="B"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq2"
+                    id="radioreq3"
+                    value="option3"
+                    label="C"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq2"
+                    id="radioreq4"
+                    value="option4"
+                    label="S"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq2"
+                    id="radioreq5"
+                    value="option5"
+                    label="F"
+                  />
+                </CCol>
+              </CRow>
+              <CRow>
+                <CCol sm={5}>
+                  <CFormSelect size="sm" className="mb-2" aria-label="Small select example">
+                    <option>Select a Subject</option>
+                    <option value="1">Combined Mathematics</option>
+                    <option value="2">Chemistry</option>
+                    <option value="3">Physics</option>
+                    <option value="4">ICT</option>
+                    <option value="5">Bio Science</option>
+                    <option value="6">Agriculture</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol sm={8}>
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq3"
+                    id="radioreq1"
+                    value="option1"
+                    label="A"
+                    defaultChecked
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq3"
+                    id="radioreq2"
+                    value="option2"
+                    label="B"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq3"
+                    id="radioreq3"
+                    value="option3"
+                    label="C"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq3"
+                    id="radioreq4"
+                    value="option4"
+                    label="S"
+                  />
+                  <CFormCheck
+                    inline
+                    type="radio"
+                    name="radioresultreq3"
+                    id="radioreq5"
+                    value="option5"
+                    label="F"
+                  />
+                </CCol>
+              </CRow>
+            </CRow>
+            <CRow className="mb-3">
+              <CFormLabel htmlFor="inputdescrip" className="col-sm-3 col-form-label">
+                Description
+              </CFormLabel>
+              <CCol sm={12}>
+                <CFormTextarea id="exampleFormControlTextarea1" rows="5"></CFormTextarea>
+              </CCol>
+            </CRow>
+
+            {/* <fieldset className="row mb-3">
+              <legend className="col-form-label col-sm-2 pt-0">Radios</legend>
+              <CCol sm={10}>
+                <CFormCheck
+                  type="radio"
+                  name="gridRadios"
+                  id="gridRadios1"
+                  value="option1"
+                  label="First radio"
+                  defaultChecked
+                />
+                <CFormCheck
+                  type="radio"
+                  name="gridRadios"
+                  id="gridRadios2"
+                  value="option2"
+                  label="Second radio"
+                />
+                <CFormCheck
+                  type="radio"
+                  name="gridRadios"
+                  id="gridRadios3"
+                  value="option3"
+                  label="Third disabled radio"
+                  disabled
+                />
+              </CCol>
+            </fieldset> */}
+          </CForm>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setVisible(false)}>
+            Close
+          </CButton>
+          <CButton color="primary">Save changes</CButton>
+        </CModalFooter>
+      </CModal>
     </div>
   )
 }
