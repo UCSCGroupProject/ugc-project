f62f1b682fe712cbc99e499141bb796c3513be8a
ilmabf <2019cs061@stu.ucsc.lk>
2022-08-23 15:57:56 +0530

    Update SelectionProcess.js

--- frontend-web/src/views/staff/selection/selected/SelectionProcess.js
+++ frontend-web/src/views/staff/selection/selected/SelectionProcess.js
@@ -1,8 +1,76 @@
 import React from 'react'
+import { useState } from 'react'
+import {
+  CCard,
+  CTable,
+  CCol,
+  CTableHead,
+  CFormInput,
+  CCardBody,
+  CButton,
+  CWidgetStatsB,
+  CFormSelect,
+  CCardHeader,
+  CTableRow,
+  CInputGroupText,
+  CTableHeaderCell,
+  CInputGroup,
+  CRow,
+  CTableBody,
+  CTableDataCell,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CTabContent,
+  CTabPane,
+  CContainer,
+  CCollapse,
+} from '@coreui/react'
 
 function SelectionProcess() {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <div>SelectionProcess</div>
+    <>
+      <CContainer>
+        <CRow></CRow>
+        <CRow>
+          <CCol xs={2}></CCol>
+          <CCol xs={8}>
+            <CButton size="lg" color="warning" onClick={() => setVisible(!visible)}>
+              Run Selection Process
+            </CButton>
+            <CCard className="mt-3">
+              <CCardBody>
+                <CWidgetStatsB
+                  className="mb-3"
+                  progress={{ color: 'success', value: 100 }}
+                  text="Selection of top 40%"
+                  title="Merit Selection"
+                  value="100.0%"
+                />
+                <CWidgetStatsB
+                  className="mb-3"
+                  progress={{ color: 'warning', value: 75 }}
+                  text="Selection of remaining 55% under 25 districts "
+                  title="District Quota Selection"
+                  value="75.1%"
+                />
+                <CWidgetStatsB
+                  className="mb-3"
+                  progress={{ color: 'warning', value: 0 }}
+                  text="Selection of remaining 5% under 16 districts "
+                  title="Educationally Disadvantaged Selection"
+                  value="0%"
+                />
+              </CCardBody>
+            </CCard>
+          </CCol>
+          <CCol xs={2}></CCol>
+        </CRow>
+        <CRow></CRow>
+      </CContainer>
+    </>
   )
 }
 
