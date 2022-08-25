3bf0ffa3a11b610493f8363923847cb30c528f5a
dhanushkasandakelum <dhanushkasandakelum711@gmail.com>
2022-08-26 02:07:19 +0530

    Docker files added and tables improved

--- frontend-web/src/views/student/courses/courseDetails/AllCourses.js
+++ frontend-web/src/views/student/courses/courseDetails/AllCourses.js
@@ -22,6 +22,8 @@ import { cilSearch } from '@coreui/icons'
 import { cilFilter } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
+import AppTable from '../../../../components/table/AppTable'
+
 const allCoursesData = [
   {
     id: '1',
@@ -39,62 +41,7 @@ function AllCourses() {
           <CCard className="mb-4">
             <CCardHeader>All Courses</CCardHeader>
             <CCardBody>
-              <CRow className="py-2 bg-light rounded">
-                <CCol md={6}>
-                  <CInputGroup>
-                    <CInputGroupText>Filter By</CInputGroupText>
-                    <CFormSelect aria-label="filterByOption1">
-                      <option value="all">All</option>
-                      <option value="unicode">Unicode</option>
-                      <option value="course">Course</option>
-                      <option value="university">University</option>
-                    </CFormSelect>
-                    <CInputGroupText> in </CInputGroupText>
-                    <CFormSelect aria-label="filterByOption1">
-                      <option value="ascending">Ascending</option>
-                      <option value="descending">Descending</option>
-                    </CFormSelect>
-                    <CInputGroupText> order </CInputGroupText>
-                    <CButton color="warning" type="button" className="text-white">
-                      <CIcon icon={cilFilter} />
-                      <span>{'  '}Filter</span>
-                    </CButton>
-                  </CInputGroup>
-                </CCol>
-                <CCol md={4} className="ms-auto">
-                  <CInputGroup>
-                    <CFormInput type="text" name="phone" placeholder="Search..." />
-                    <CButton color="warning" type="button" className="text-white">
-                      <CIcon icon={cilSearch} />
-                      <span>{'  '}Search</span>
-                    </CButton>
-                  </CInputGroup>
-                </CCol>
-              </CRow>
-              <br />
-
-              <CRow className="m-1">
-                <CTable bordered>
-                  <CTableHead color="dark">
-                    <CTableRow>
-                      <CTableHeaderCell>No.</CTableHeaderCell>
-                      <CTableHeaderCell>Unicode</CTableHeaderCell>
-                      <CTableHeaderCell>Course</CTableHeaderCell>
-                      <CTableHeaderCell>University</CTableHeaderCell>
-                    </CTableRow>
-                  </CTableHead>
-                  <CTableBody>
-                    {allCoursesData.map((item) => (
-                      <CTableRow key={item.id}>
-                        <CTableHeaderCell>{item.id}</CTableHeaderCell>
-                        <CTableDataCell>{item.unicode}</CTableDataCell>
-                        <CTableDataCell>{item.courseOfStudy}</CTableDataCell>
-                        <CTableDataCell>{item.university}</CTableDataCell>
-                      </CTableRow>
-                    ))}
-                  </CTableBody>
-                </CTable>
-              </CRow>
+              <AppTable />
             </CCardBody>
           </CCard>
         </CCol>
