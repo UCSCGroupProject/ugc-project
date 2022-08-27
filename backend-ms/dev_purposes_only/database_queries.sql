9ee8141d83e01728c81296b26f3004c8c07b27f9
dhanushkasandakelum <dhanushkasandakelum711@gmail.com>
2022-08-27 17:26:25 +0530

    Docker updated

---
+++ backend-ms/dev_purposes_only/database_queries.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS student_db;
+DROP DATABASE IF EXISTS staff_db;
+DROP DATABASE IF EXISTS university_db;
+DROP DATABASE IF EXISTS school_db;
+DROP DATABASE IF EXISTS notification_db;
+DROP DATABASE IF EXISTS email_db;
+DROP DATABASE IF EXISTS zscore_db;
+
+CREATE DATABASE student_db;
+CREATE DATABASE staff_db;
+CREATE DATABASE university_db;
+CREATE DATABASE school_db;
+CREATE DATABASE notification_db;
+CREATE DATABASE email_db;
+CREATE DATABASE zscore_db;
+
+
