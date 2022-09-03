7cb7495af649a65ab4be3535eeb877ac47c3788f
Tharushi Algama <92346310+TharushiAlgama@users.noreply.github.com>
2022-09-03 16:19:09 +0700

    contact us page updated

--- frontend-web/src/views/user/About.js
+++ frontend-web/src/views/user/About.js
@@ -1,89 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { CContainer, CRow, CCol } from '@coreui/react'
-
 
 import LatestHighlights from '../user/home/components/LatestHighlights'
 import NewsAndEvents from '../user/home/components/NewsAndEvents'
 import LatestCirculars from '../user/home/components/LatestCirculars'
 
 function About() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+
+const send =()=>{
+  alert("Form submitted by " + name)
+}
+
+
+
+
   return (
     <div className="body flex-grow-1 custom-wrapper">
       <div>
         <CContainer>
           <CRow>
             <CCol md={9}>
-
               <br />
               <h3>About Us</h3>
-    
-    <p className="lead">
-      The University Grants Commission (UGC) is the apex body of the University System in
-      Sri Lanka which was established on 22nd December 1978 under the Universities Act No.
-      16 of 1978. The functions of the UGC are; planning and coordination of university
-      education, allocation of funds to Higher Educational Institutions (HEIs),
-      maintenance of academic standards, regulation of the administration of HEIs and
-      regulation of admission of students to HEIs.{' '}
-    </p>
 
-<h5>Our Vision</h5>
-  <p className="lead">
-      <b>Provide Knowledge Leadership for a Better Sri Lanka / World</b>
-  </p>
+              <p className="lead">
+                The University Grants Commission (UGC) is the apex body of the University System in
+                Sri Lanka which was established on 22nd December 1978 under the Universities Act No.
+                16 of 1978. The functions of the UGC are; planning and coordination of university
+                education, allocation of funds to Higher Educational Institutions (HEIs),
+                maintenance of academic standards, regulation of the administration of HEIs and
+                regulation of admission of students to HEIs.{' '}
+              </p>
 
-<h5>Our Mission</h5>
-  <p className="lead">
-      To foster management and good governance in facilitating the provision of undergraduate, postgraduate and professional education of highest quality with high impact research, quality teaching and industry engagement through a coordinated system of State Universities and HEIs, to create knowledge leaders who are passionate about meeting the triple bottom line.
-  </p>
+              <h5>Our Vision</h5>
+              <p className="lead">
+                <b>Provide Knowledge Leadership for a Better Sri Lanka / World</b>
+              </p>
 
-<br/>
+              <h5>Our Mission</h5>
+              <p className="lead">
+                To foster management and good governance in facilitating the provision of
+                undergraduate, postgraduate and professional education of highest quality with high
+                impact research, quality teaching and industry engagement through a coordinated
+                system of State Universities and HEIs, to create knowledge leaders who are
+                passionate about meeting the triple bottom line.
+              </p>
 
-  <h3>Contact Us</h3>
-  <br/>
-
-  <address>
-    <strong>Email: ugcadministrator@gmail.com</strong>
-    <br/>
-    <br/>
-    <p>
-      <strong>Phone: +94 xxx xxxxxxx</strong>
-    </p>
-  </address>
-  <hr/>
-  <form>
-    <input 
-    className="form-control"
-    id="name"
-    name="name"
-    placeholder="Name"
-    type="text"/>
-    
-    <br/>
-
-    <input 
-    className="form-control"
-    id="email"
-    name="email"
-    placeholder="Email"
-    type="email"
-    />
-
-    <br/>
-
-    <textarea className="form-control " 
-    id="message"
-    name="message"
-    placeholder="message"
-    rows='5'
-    ></textarea>
-
-  <br/>
-
-<button className="btn ac_btn" type="submit">Send</button>
-  </form>
               <br />
 
+              <h3>Contact Us</h3>
+              <br />
+
+              <address>
+                <strong>Email: ugcadministrator@gmail.com</strong>
+                <br />
+                <br />
+                <p>
+                  <strong>Phone: +94 xxx xxxxxxx</strong>
+                </p>
+              </address>
+              <hr />
+
+              <CRow>
+                <CCol md={8}>
+                  <div>
+                    <form >
+                      <input
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        type="text"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+
+                      <br />
+
+                      <input
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+
+                      <br />
+
+                      <textarea
+                        className="form-control "
+                        id="message"
+                        name="message"
+                        placeholder="message"
+                        rows="5"
+                        onChange={(e) => setMessage(e.target.value)}
+                      ></textarea>
+
+                      <br />
+
+                      <button className="btn btn-dark" type="submit" onClick={send}>
+                        Send
+                      </button>
+                    </form>
+                  </div>
+                </CCol>
+              </CRow>
+
+              <br />
             </CCol>
 
             <CCol md={3}>
